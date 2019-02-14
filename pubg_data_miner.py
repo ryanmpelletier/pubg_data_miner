cade30aa54c9067b2ddaee1484ec6f90f007cc3b
ryanp102694 <ryanmatthewpelletier@gmail.com>
2019-02-13 20:44:48 -0500

    initial commit

--- pubg_data_miner.py
+++ pubg_data_miner.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-SHARD = "steam"
-PLAYER_NAMES = "supernewb,BarbTheBarb,Italian2007"
-API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxZWE1YzViMC0xOTVlLTAxMzYtNWZkMC00ZmM4YmMzYjI1ZjkiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTIyNzU0MDc3LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1YmdzbWFjayIsInNjb3BlIjoiY29tbXVuaXR5IiwibGltaXQiOjEwfQ.H2kCPsmc9XLPYa_-3luNh2hvUpmcV6OGoJevql7BBgM"
-PLAYER_REQUEST_URL = "https://api.pubg.com/shards/" + SHARD + "/players?filter[playerNames]=" + PLAYER_NAMES
+PLATFORM = "steam"
+PLAYER_NAMES = "supernewb"
+API_KEY = "YOUR_API_KEY"
+PLAYER_REQUEST_URL = "https://api.pubg.com/shards/" + PLATFORM + "/players?filter[playerNames]=" + PLAYER_NAMES
 API_REQUEST_HEADERS = {
     "Authorization": ("Bearer " + API_KEY),
     "Accept": "application/vnd.api+json"
@@ -34,7 +34,7 @@ def main():
     telemetry_asset_ids = set()
     telemetry_objects = list()
     for match_id in potentially_new_match_ids:
-        match_request_url="https://api.pubg.com/shards/" + SHARD + "/matches/" + match_id
+        match_request_url="https://api.pubg.com/shards/" + PLATFORM + "/matches/" + match_id
         match_response = requests.get(match_request_url, headers=API_REQUEST_HEADERS).json()
         for asset in match_response["data"]["relationships"]["assets"]["data"]:
             telemetry_asset_ids.add(asset["id"])
