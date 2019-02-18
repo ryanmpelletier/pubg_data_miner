b5662a61c05ee342eacf2b7c5148061a39a37725
ryanp102694 <ryanmatthewpelletier@gmail.com>
2019-02-18 01:04:22 -0500

    pubg_data_miner.py

--- pubg_data_miner.py
+++ pubg_data_miner.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import os
+
 
 PLATFORM = "steam"
 PLAYER_NAMES = "YOUR_PUBG_USERNAME"
@@ -11,8 +13,6 @@ API_REQUEST_HEADERS = {
 }
 
 MATCHES_FILE = "matches.txt"
-TELEMETRY_URLS_FILE = "telemetries.txt"
-TELEMETRY_DIR = "telemetries"
 
 def main():
     player_response = requests.get(PLAYER_REQUEST_URL, headers=API_REQUEST_HEADERS).json()["data"]
@@ -32,26 +32,25 @@ def main():
 
     #let's get all the telemetry urls
     telemetry_asset_ids = set()
-    telemetry_objects = list()
     for match_id in potentially_new_match_ids:
         match_request_url="https://api.pubg.com/shards/" + PLATFORM + "/matches/" + match_id
         match_response = requests.get(match_request_url, headers=API_REQUEST_HEADERS).json()
+        match_attributes = match_response["data"]["attributes"]
+
         for asset in match_response["data"]["relationships"]["assets"]["data"]:
             telemetry_asset_ids.add(asset["id"])
 
         for item in match_response["included"]:
             if item["id"] in telemetry_asset_ids:
-                telemetry_objects.append(item)
-
-    #now I can go grab data for the new_match_ids using my telemetry URLs
-    for telemetry_object in telemetry_objects:
-        telemetry = requests.get(telemetry_object["attributes"]["URL"]).json()
-        telemetry_file = open(telemetry_object["id"], 'w')
-        telemetry_file.write(json.dumps(telemetry, ensure_ascii=False))
-
-    #then I can write those matches I grabbed data for to a file
-    for match_id in potentially_new_match_ids:
-        previous_match_file.write(match_id + "\n")
+                #lets grab the telemetry data now
+                telemetry = requests.get(item["attributes"]["URL"]).json()
+                if not os.path.exists(match_attributes["mapName"]):
+                    os.makedirs(match_attributes["mapName"])
+                if not os.path.exists(os.path.join(match_attributes["mapName"], match_attributes["gameMode"])):
+                    os.makedirs(os.path.join(match_attributes["mapName"], match_attributes["gameMode"]))
+                telemetry_file = open(os.path.join(match_attributes["mapName"], match_attributes["gameMode"], item["id"]), 'w')
+                telemetry_file.write(json.dumps(telemetry, ensure_ascii=False))
+                previous_match_file.write(match_id + "\n")
 
 
 if __name__ == "__main__": main()
